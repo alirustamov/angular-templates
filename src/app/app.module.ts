fa91436f68fe8501d3425d444cb9e2cad3aafe82
Ali <ali.a.rustamov@gmail.com>
2019-08-26 13:00:20 +0600

    [feat] added digit-only directive for inputs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DigitOnlyModule } from './directives/digit-only/digit-only.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    DigitOnlyModule
   ],
   providers: [],
   bootstrap: [AppComponent]
